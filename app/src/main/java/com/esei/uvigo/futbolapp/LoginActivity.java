c2c393ede4ac0b2a31046ffca85b7ed037de845c
guillelandin1 <guillelandin1@gmail.com>
2025-02-22 19:58:52 +0100

    LoginActivity and RegisterActivity structure DONE

--- app/src/main/java/com/esei/uvigo/futbolapp/LoginActivity.java
+++ app/src/main/java/com/esei/uvigo/futbolapp/LoginActivity.java
@@ -1,4 +1,55 @@
 package com.esei.uvigo.futbolapp;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private FutbolFacade futbolFacade;
+    private EditText edtUsername,edtPassword;
+    private Button btnLogin, btnRecoverPassword;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //Referenciamos a los elementos del layout
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRecoverPassword = findViewById(R.id.btnResetPass);
+
+        //Manejo boton Login
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = edtUsername.getText().toString().trim();
+                String password = edtPassword.getText().toString().trim();
+
+                if(username.isEmpty() || password.isEmpty()){
+                    Toast.makeText(LoginActivity.this,"Por favor cubra ambos campos ", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //TODO -> Gestionar la autentificación de Usuario y Contraseña
+
+            }
+        });
+
+        btnRecoverPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO -> Gestionar el envío de token para recuperar contraseña
+            }
+        });
+
+
+    }
 }
--- app/src/main/java/com/esei/uvigo/futbolapp/RegisterActivity.java
+++ app/src/main/java/com/esei/uvigo/futbolapp/RegisterActivity.java
@@ -1,4 +1,51 @@
 package com.esei.uvigo.futbolapp;
 
-public class RegisterActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FutbolFacade futbolFacade;
+    private EditText edtUsername,edtPassword,edtEmail;
+    private Button btnRegister;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        futbolFacade = new FutbolFacade((FutbolApplication) getApplication(), this);
+
+        //Referenciamos los elementos del Layout para trabajar con ellos
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtEmail = findViewById(R.id.edtEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+
+
+       btnRegister.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v){
+               String username = edtUsername.getText().toString().trim();
+               String password = edtPassword.getText().toString().trim();
+               String email = edtEmail.getText().toString().trim();
+
+               if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+                   Toast.makeText(RegisterActivity.this, "Por favor complete todos los campos", Toast.LENGTH_SHORT).show();
+                   return;
+               } else {
+                   Toast.makeText(RegisterActivity.this, "Error al registrar usuario", Toast.LENGTH_SHORT).show();
+               }
+           }
+
+       });
+    }
 }
